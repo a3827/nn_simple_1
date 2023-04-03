03c3829927432c0f9e13bdebf14c8e06dad8f593
arek <areklib@poczta.onet.pl>
2023-04-03 15:44:41 +0200

    Added debo.py, notes.txt, sahi.py.

---
+++ debo.py
@@ -0,0 +1,3 @@
+# This is step by step design, probably better then main.py from:
+# https://realpython.com/python-ai-neural-network/
+#
---
+++ sahi.py
@@ -0,0 +1,3 @@
+# This example, maybe not so good as Deborah, is from  Sahishanu:
+# https://www.geeksforgeeks.org/implementation-of-neural-network-from-scratch-using-numpy/
+#
